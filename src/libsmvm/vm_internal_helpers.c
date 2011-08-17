fdeb087930d900901c55431ae8b9ee7fc81dbf72
Jaak Ristioja <jaak.ristioja@cyber.ee>
2011-08-17 12:39:08 +0300

    Small optimization.

--- src/libsmvm/vm_internal_helpers.c
+++ src/libsmvm/vm_internal_helpers.c
@@ -335,8 +335,8 @@ int SMVM_Program_endPrepare(struct SMVM_Program * const p) {
 
     /* Initialize global frame: */
     SMVM_StackFrame_init(p->globalFrame, NULL);
-    p->globalFrame->returnValueAddr = &(p->returnValue);
     p->globalFrame->returnAddr = NULL; /* Triggers halt on return. */
+    /* p->globalFrame->returnValueAddr = &(p->returnValue); is not needed. */
 
     p->thisFrame = p->globalFrame;
     p->nextFrame = NULL;
