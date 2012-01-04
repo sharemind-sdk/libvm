63eaf7c118cb57447c2c5c70a3722019b27e8c99
Jaak Ristioja <jaak.ristioja@cyber.ee>
2012-01-04 13:10:09 +0200

    Fixed compile error.

--- src/libsmvm/vm_internal_helpers.h
+++ src/libsmvm/vm_internal_helpers.h
@@ -123,7 +123,7 @@ inline SMVM_MemorySlot_init_DEFINE
 inline SMVM_MemorySlot_destroy_DEFINE
 
 SM_MAP_DECLARE(SMVM_MemoryMap,uint64_t,const uint64_t,SMVM_MemorySlot,inline)
-SM_MAP_DEFINE(SMVM_MemoryMap,uint64_t,const uint64_t,SMVM_MemorySlot,(uint16_t),SM_MAP_KEY_EQUALS_uint64_t,SM_MAP_KEY_LESS_THAN_uint64_t,SM_MAP_KEYCOPY_REGULAR,SM_MAP_KEYFREE_REGULAR,malloc,free,inline)
+SM_MAP_DEFINE(SMVM_MemoryMap,uint64_t,const uint64_t,SMVM_MemorySlot,(uint16_t)(key),SM_MAP_KEY_EQUALS_uint64_t,SM_MAP_KEY_LESS_THAN_uint64_t,SM_MAP_KEYCOPY_REGULAR,SM_MAP_KEYFREE_REGULAR,malloc,free,inline)
 #else
 SMVM_MemorySlot_init_DECLARE;
 SMVM_MemorySlot_destroy_DECLARE;
