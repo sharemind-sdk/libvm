sharemind-sdk/libvm

commit 2a0b3def8b46a6ef6793790b89f2407c02fe03fd

Fixed missing PDPI cache initialization.

src/sharemind/libvm/process.c
@@ -103,7 +103,20 @@ SharemindProcess * SharemindProcess_new(SharemindProgram * program) {
     assert(p->bssSections.size == program->bssSectionSizes.size);
 
 
+    /* Initialize PDPI cache: */
     SharemindPdpiCache_init(&p->pdpiCache);
+    for (size_t i = 0u; i < program->pdBindings.size; i++) {
+        SharemindPdpiCacheItem * ci = SharemindPdpiCache_push(&p->pdpiCache);
+        if (!ci)
+            goto SharemindProcess_new_fail_pdpiCache;
+
+        if (!SharemindPdpiCacheItem_init(ci, program->pdBindings.data[i])) {
+            if (!ci->pdpi)
+                SharemindPdpiCache_pop(&p->pdpiCache);
+            goto SharemindProcess_new_fail_pdpiCache;
+        }
+    }
+    assert(p->pdpiCache.size == program->pdBindings.size);
 
     SharemindFrameStack_init(&p->frames);
 
@@ -156,6 +169,10 @@ SharemindProcess * SharemindProcess_new(SharemindProgram * program) {
 
     return p;
 
+SharemindProcess_new_fail_pdpiCache:
+
+    SharemindPdpiCache_destroy_with(&p->pdpiCache, &SharemindPdpiCacheItem_destroy);
+
 SharemindProcess_new_fail_bss_sections:
 
     SharemindDataSectionsVector_destroy_with(&p->bssSections, &SharemindDataSection_destroy);
@@ -189,7 +206,7 @@ static inline void SharemindProcess_destroy(SharemindProcess * p) {
 
     SharemindFrameStack_destroy_with(&p->frames, &SharemindStackFrame_destroy);
 
-    SharemindPdpiCache_destroy(&p->pdpiCache);
+    SharemindPdpiCache_destroy_with(&p->pdpiCache, &SharemindPdpiCacheItem_destroy);
 
     SharemindDataSectionsVector_destroy_with(&p->bssSections, &SharemindDataSection_destroy);
     SharemindDataSectionsVector_destroy_with(&p->dataSections, &SharemindDataSection_destroy);

src/sharemind/libvm/pdpicache.h
@@ -33,6 +33,38 @@ typedef struct {
     void * moduleHandle;
 } SharemindPdpiCacheItem;
 
+static inline bool SharemindPdpiCacheItem_init(SharemindPdpiCacheItem * ci,
+                                               SharemindPd * pd)
+        __attribute__ ((nonnull(1, 2), warn_unused_result));
+static inline bool SharemindPdpiCacheItem_init(SharemindPdpiCacheItem * ci,
+                                               SharemindPd * pd)
+{
+    assert(ci);
+    assert(pd);
+    ci->pdpi = SharemindPdpi_new(pd);
+    if (!ci->pdpi)
+        return false;
+
+    if (!SharemindPdpi_start(ci->pdpi))
+        return false;
+
+    ci->pdpiHandle = SharemindPdpi_get_handle(ci->pdpi);
+    SharemindPdk * pdk = SharemindPd_get_pdk(pd);
+    assert(pdk);
+    ci->pdkIndex = SharemindPdk_get_index(pdk);
+    SharemindModule * module = SharemindPdk_get_module(pdk);
+    assert(module);
+    ci->moduleHandle = SharemindModule_get_handle(module);
+    return true;
+}
+
+static inline void SharemindPdpiCacheItem_destroy(SharemindPdpiCacheItem * ci) __attribute__ ((nonnull(1)));
+static inline void SharemindPdpiCacheItem_destroy(SharemindPdpiCacheItem * ci) {
+    assert(ci);
+    assert(ci->pdpi);
+    SharemindPdpi_free(ci->pdpi);
+}
+
 SHAREMIND_VECTOR_DECLARE(SharemindPdpiCache,SharemindPdpiCacheItem,,static inline)
 SHAREMIND_VECTOR_DEFINE(SharemindPdpiCache,SharemindPdpiCacheItem,malloc,free,realloc,static inline)
 

commit 733a1e5beb415f53cf5b5d0fcf7df2b300b57b3f

SharemindProcess: Fixed initialization rollback bug introduced in 2a0b3def.

src/sharemind/libvm/process.c
@@ -122,8 +122,7 @@ static SharemindVmError SharemindProcess_init(SharemindProcess * p,
         }
 
         if (!SharemindPdpiCacheItem_init(ci, program->pdBindings.data[i])) {
-            if (!ci->pdpi)
-                SharemindPdpiCache_pop(&p->pdpiCache);
+            SharemindPdpiCache_pop(&p->pdpiCache);
             error = SHAREMIND_VM_OUT_OF_MEMORY;
             goto SharemindProcess_new_fail_pdpiCache;
         }