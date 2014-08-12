9b47bc1292a332bfd38eda3da75a6e41866a8010
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-08-12 12:48:56 +0300

    SharemindStackFrame: Small restrict optimization.

--- src/sharemind/libvm/stackframe.h
+++ src/sharemind/libvm/stackframe.h
@@ -40,11 +40,11 @@ struct SharemindStackFrame_ {
 typedef struct SharemindStackFrame_ SharemindStackFrame;
 
 static inline void SharemindStackFrame_init(
-        SharemindStackFrame * const f,
-        SharemindStackFrame * const prev) __attribute__ ((nonnull(1)));
+        SharemindStackFrame * const restrict f,
+        SharemindStackFrame * const restrict prev) __attribute__ ((nonnull(1)));
 static inline void SharemindStackFrame_init(
-        SharemindStackFrame * const f,
-        SharemindStackFrame * const prev)
+        SharemindStackFrame * const restrict f,
+        SharemindStackFrame * const restrict prev)
 {
     assert(f);
     SharemindRegisterVector_init(&f->stack);
