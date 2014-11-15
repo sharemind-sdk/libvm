7bdad989d673f7a20afe670293719a42eabaa7d7
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-11-16 00:33:47 +0200

    Fixed crashes caused by frame stack using vector.

--- src/framestack.h
+++ src/framestack.h
@@ -15,43 +15,20 @@
 #endif
 
 #include <sharemind/extern_c.h>
-#include <sharemind/vector.h>
+#include <sharemind/stack.h>
 #include <stdlib.h>
 #include "stackframe.h"
 
 SHAREMIND_EXTERN_C_BEGIN
 
-SHAREMIND_VECTOR_DEFINE_BODY(SharemindFrameStack,SharemindStackFrame,)
-SHAREMIND_VECTOR_DECLARE_INIT(SharemindFrameStack,
-                              inline,
-                              SHAREMIND_COMMA visibility("internal"))
-SHAREMIND_VECTOR_DEFINE_INIT(SharemindFrameStack, inline)
-SHAREMIND_VECTOR_DECLARE_DESTROY(SharemindFrameStack,
-                                 inline,
-                                 SHAREMIND_COMMA visibility("internal"))
-SHAREMIND_VECTOR_DEFINE_DESTROY_WITH(SharemindFrameStack,
-                                     inline,
-                                     SharemindStackFrame,,
-                                     free,
-                                     SharemindStackFrame_destroy(value);)
-SHAREMIND_VECTOR_DECLARE_FORCE_RESIZE(SharemindFrameStack,
-                                      inline,
-                                      SHAREMIND_COMMA visibility("internal"))
-SHAREMIND_VECTOR_DEFINE_FORCE_RESIZE(SharemindFrameStack,
-                                     inline,
-                                     SharemindStackFrame,
-                                     realloc)
-SHAREMIND_VECTOR_DECLARE_PUSH(SharemindFrameStack,
-                              inline,
-                              SharemindStackFrame,
-                              SHAREMIND_COMMA visibility("internal"))
-SHAREMIND_VECTOR_DEFINE_PUSH(SharemindFrameStack,
-                             inline,
-                             SharemindStackFrame)
-SHAREMIND_VECTOR_DECLARE_POP(SharemindFrameStack,
-                             inline,
-                             SHAREMIND_COMMA visibility("internal"))
-SHAREMIND_VECTOR_DEFINE_POP(SharemindFrameStack, inline)
+SHAREMIND_STACK_DECLARE(SharemindFrameStack,
+                        SharemindStackFrame,,
+                        static inline)
+SHAREMIND_STACK_DEFINE(SharemindFrameStack,
+                       SharemindStackFrame,
+                       malloc,
+                       free,
+                       static inline)
 
 SHAREMIND_EXTERN_C_END
 
