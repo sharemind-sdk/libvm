sharemind-sdk/libvm

commit 60adc0ac13ef0e3cf8485a578111c95d1a0b8915

ProcessState: Small simplification and noexcept optimization.

src/Process.cpp
@@ -326,7 +326,7 @@ void ProcessState::resume() {
 
 void ProcessState::execute(ExecuteMethod const executeMethod) {
     auto const setState =
-            [this](State const newState) {
+            [this](State const newState) noexcept {
                 RUNSTATEGUARD;
                 assert(m_state == State::Running);
                 m_state = newState;
@@ -334,16 +334,15 @@ void ProcessState::execute(ExecuteMethod const executeMethod) {
 
     try {
         vmRun(executeMethod, this);
+        m_pdpiCache.stopPdpis();
+        setState(State::Finished);
     } catch (Process::TrapException const &) {
         setState(State::Trapped);
         throw;
     } catch (...) {
-        m_pdpiCache.stopPdpis();
         setState(State::Crashed);
         throw;
     }
-    m_pdpiCache.stopPdpis();
-    setState(State::Finished);
 }
 
 void ProcessState::pause() noexcept

commit 1d7ee48eef060bf6eda35d0075dcfc44bf687467

Process: Fixed PDPI-s not being stopped after crash.
Bug was introduced in 60adc0ac13ef0e3cf8485a578111c95d1a0b8915.

src/Process.cpp
@@ -337,15 +337,16 @@ void ProcessState::execute(ExecuteMethod const executeMethod) {
 
     try {
         vmRun(executeMethod, this);
-        m_pdpiCache.stopPdpis();
-        setState(State::Finished);
     } catch (Process::TrapException const &) {
         setState(State::Trapped);
         throw;
     } catch (...) {
+        m_pdpiCache.stopPdpis();
         setState(State::Crashed);
         throw;
     }
+    m_pdpiCache.stopPdpis();
+    setState(State::Finished);
 }
 
 void ProcessState::pause() noexcept